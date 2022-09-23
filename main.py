a8755de9a4e4cb3fd58e946f0216dab4e0d59a43
user <fsdf@>
2022-09-23 16:28:01 +0500

    123132

---
+++ main.py
@@ -0,0 +1,13 @@
+def simple(n):
+    sieve = list(range(n + 1))
+    sieve[1] = 0
+    for i in sieve:
+        if i > 1:
+            for j in range(2*i, len(sieve), i):
+                sieve[j] = 0
+    return sieve
+
+def main():
+    print(simple(15))
+
+main()
